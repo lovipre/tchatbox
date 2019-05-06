55e4e4c4e32e1afb8b1726ca7e0f3102dabe594b
max <max.demetz@hotmail.com>
2019-05-06 08:58:30 +0200

    max

--- creation.php
+++ creation.php
@@ -1,53 +1,5 @@
 <?php 
 include 'bdd.php';
-
-
-if($_POST && $_GET){
-
-    // je récupère le champ du formulaire, je le met en majuscule et le stock dans une variable.
-    $nomSalle = strtoupper($_POST['inputIdRoom']);
-    //je remplace les espcaces par des -
-    $nomSalle = str_replace(" ","-",$nomSalle);
-    echo($nomSalle);
-
-
-    // si le champ du formulaire est vide au moment du submit
-    if ($nomSalle == '' ){
-        //J'affiche un message d'erreur
-
-        $erreur == true ;
-        $message = "Erreur : Veuillez donner un nom à cette nouvelle discussion";
-        echo($message);
-
-    //Sinon  
-    } else {
-        //je récupère les infos de la salle dans la base de données.
-        $requete= $pdo->prepare("SELECT * FROM Salle WHERE NomSalle=?");
-        $requete->execute([$nomSalle]);
-
-        //on récupère les infos retournées par la BDD sous forme de tableau 
-        $result = $requete->fetch();
-        //Si le résultat n'est pas un tableau vide
-        if (empty($result) == true){
-
-            //Je peux creer une nouvelle discussion
-            $requete= $pdo->prepare("INSERT INTO Salle(Id_CreateurSalle,NomSalle) VALUES (?,?)");
-            $requete->execute([$_GET['id'],$nomSalle]);
-
-            //Je récupère l'Id de la nouvelle discussion
-            $requete = $pdo->prepare("SELECT Id FROM Salle WHERE NomSalle=?");
-            $requete->execute([$nomSalle]);
-            $result = $requete->fetch();
-
-            //Redirection vers la page classe.php
-            header("location: salle.php?id_Salle=".$_GET['id']."&id_User=".$result['Id']."");
-            exit();
-
-        //Sinon, j'affiche un message d'erreur   
-        } else {
-            echo("Erreur : Cette discussion existe déjà, merci de lui donner un nouveau nom");
-        }
-
-    }
-}
 include "creation.phtml";
+
+
--- js/creation.js
+++ js/creation.js
@@ -1,2 +1,53 @@
 'use strict'
+//au chargement de la page
+$(document).ready(function(){
+    //je mets un gestionnaire d'événement sur le boutton du formulaire
+    $("#creation").click(creerSalle);
+});
+
+function creerSalle(e){
+    //j'annule l'action par défaut du submit
+    e.preventDefault();
+    
+    //si l'url ne contient pas l'id de l'utilisateur
+    if ( $_GET('id') == null ){
+        //J'affiche un message d'erreur
+        $("#errorMessage").html("Oups, <br> désolé, je ne vous ai pas reconnu...<br> il faut commencer par vous identifier");
+        //je cache l'input du formulaire
+        $("#inputIdRoom").addClass("hide");
+        //je cache le boutton du formulaire
+        $("#creation").addClass("hide");
+        //je modifie le lien pour rediriger vers la page de connexion
+        $("#rejoindre").attr('href','index.php');
+        //je change le texte du lien
+        $("#rejoindre").html("S'identifier");
+
+    } else {
+        //je stock l'id dans une variable
+        var userId = $_GET('id');
+        //je recupere la valeur du champ du formulaire
+        var nomDiscussion = $("#inputIdRoom").val();
+        console.log(nomDiscussion);
+        //si le champ est vide
+        if (nomDiscussion==""){
+            //j'affiche un message d'erreur
+            $("#errorMessage").html("Erreur :<br> Veuillez saisir le nom de votre nouvelle discussion");
+
+        } else { 
+            //je fais un appel AJAX
+            $.ajax({
+                url: "creationTraitement.php",//url de la page
+                method: 'post',
+                data: {nomDiscussion:nomDiscussion, userId:userId},
+                dataType: 'json', 
+                success: function(data){
+                    console.log(data);
+                },
+                error: function(){
+                    console.log('erreur');
+                }
+            });
+        }
+    }
+}
 
