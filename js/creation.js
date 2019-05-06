f0007e6a46763a73addba9996d48b9a1501fe06f
max <max.demetz@hotmail.com>
2019-05-06 11:59:13 +0200

    max

--- js/creation.js
+++ js/creation.js
@@ -3,35 +3,56 @@
 $(document).ready(function(){
     //je mets un gestionnaire d'événement sur le boutton du formulaire
     $("#creation").click(creerSalle);
-    $("#rejoindre").click(rejoindreSalle);
+    $("#rejoindre").click(redirection);
+    $("#boutonRejoindre").click(rejoindreSalle);
+    $("#lienCreation").click(redirectionCreer);
 });
 
-function rejoindreSalle(e){
-    //j'annule l'action par défaut du submit
+function redirectionCreer(e){
     e.preventDefault();
-    
+    //je stock l'id dans une variable
+    var userId = $_GET('id');
+
     //si l'url ne contient pas l'id de l'utilisateur
     if ( $_GET('id') == null ){
         //J'affiche un message d'erreur
         $("#errorMessage").html("Oups, <br> désolé, je ne vous ai pas reconnu...<br> il faut commencer par vous identifier");
-        //je cache l'input du formulaire
-        $("#inputIdRoom").addClass("hide");
-        //je cache le boutton du formulaire
-        $("#creation").addClass("hide");
-        //je modifie le lien pour rediriger vers la page de connexion
-        $("#identifier").attr('href','index.php');
-        //je change le texte du lien
-        $("#identifier").html("S'identifier");
-        //J'affiche le bouton s'identifier
-        $("#identifier").removeClass("hide");
-        //Je cache le bouton rejoindre discussion
-        $("#rejoindre").addClass("hide");
+        
 
-    } else {
+    } else{
+        //Je redirige vers la salle avec l'id correspondant
+    window.location.replace("creation.php?id="+userId);
+    }
+
+}
+
+
+function redirection(e){
+    e.preventDefault();
+    //je stock l'id dans une variable
+    var userId = $_GET('id');
+
+    //si l'url ne contient pas l'id de l'utilisateur
+    if ( $_GET('id') == null ){
+        //J'affiche un message d'erreur
+        $("#errorMessage").html("Oups, <br> désolé, je ne vous ai pas reconnu...<br> il faut commencer par vous identifier");
+
+    } else{
+        //Je redirige vers la salle avec l'id correspondant
+    window.location.replace("rejoindre.php?id="+userId);
+    }
+    
+}
+
+
+function rejoindreSalle(e){
+    e.preventDefault();
+    //je stock l'id dans une variable
+    
         //je stock l'id dans une variable
         var userId = $_GET('id');
         //je recupere la valeur du champ du formulaire
-        var nomDiscussion = $("#inputIdRoom").val();
+        var nomDiscussion = $("#inputRoom").val();
         console.log(nomDiscussion);
         //si le champ est vide
         if (nomDiscussion==""){
@@ -48,13 +69,15 @@ function rejoindreSalle(e){
                 success: function(data){
                     console.log("ok");
                     console.log(data);
-                    //Je redirige vers lsalle avec l'id correspondant
-                    window.location.replace("salle.php?id_Salle="+data[0]+"&id_User="+data[1]);
+                    
+                    //Je redirige vers l asalle avec l'id correspondant
+                    window.location.replace("salle.php?id_Salle="+data[0]['Id']+"&id_User="+userId);
+                    
 
                 }
             });
         }
-    }
+    
 }
 
 
